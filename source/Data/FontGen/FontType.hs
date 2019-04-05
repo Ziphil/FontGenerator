22d4e5fffb23a611f407a64d7b9f195bc20cfab7
Ziphil <yfe23457@nifty.com>
2019-04-05 21:18:39 +0900

    変更

--- source/Data/FontGen/FontType.hs
+++ source/Data/FontGen/FontType.hs
@@ -13,4 +13,6 @@ import qualified Data.Map as Map
 
 
 data FontMetrics = FontMetrics {em :: Double, ascent :: Double, descent :: Double}
+  deriving (Eq, Show)
+
 data FontInfo = FontInfo {family :: String, weight :: String, version :: String, metrics :: FontMetrics, glyphs :: Glyphs}
