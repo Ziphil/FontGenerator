d784ecf36826774fef4a191d767e75c42e329994
Ziphil <yfe23457@nifty.com>
2019-04-14 16:10:47 +0900

    文字の角度や幅を表す型を追加

--- source/Data/FontGen/FontType.hs
+++ source/Data/FontGen/FontType.hs
@@ -3,6 +3,8 @@
 
 module Data.FontGen.FontType
   ( Weight (..)
+  , Style (..)
+  , Stretch (..)
   , FontInfo (..)
   )
 where
@@ -12,6 +14,12 @@ import Data.Version
 
 
 data Weight = Thin | ExtraLight | Light | Regular | Medium | SemiBold | Bold | ExtraBold | Heavy
+  deriving (Eq, Show, Enum)
+
+data Style = Upright | Oblique | Italic
+  deriving (Eq, Show, Enum)
+
+data Stretch = Compressed | Condensed | Normal | Extended
   deriving (Eq, Show, Enum)
 
 data FontInfo = FontInfo {family :: String, weight :: Weight, version :: Version, metrics :: Metrics, glyphs :: Glyphs}
