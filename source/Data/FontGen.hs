59a21daaf1a33398e15fe790bffdee98f4650fa8
Ziphil <yfe23457@nifty.com>
2019-04-18 01:09:34 +0900

    デフォルトでインポートするパッケージを追加

--- source/Data/FontGen.hs
+++ source/Data/FontGen.hs
@@ -4,10 +4,12 @@
 module Data.FontGen
   ( module Data.FontGen.GlyphType
   , module Data.FontGen.Util
+  , module Diagrams.TwoD.Segment
   , module Diagrams.Prelude
   )
 where
 
 import Data.FontGen.GlyphType
 import Data.FontGen.Util
+import Diagrams.TwoD.Segment
 import Diagrams.Prelude hiding ((<~), (~~))
