beb78e7e49285471bbbe0a809fb8d56130753116
Ziphil <yfe23457@nifty.com>
2019-04-22 00:59:26 +0900

    フォント生成のインターフェースを作成

--- application/Main.hs
+++ application/Main.hs
@@ -4,28 +4,17 @@
 module Main where
 
 import Data.FontGen
-import Data.FontGen.FontType
 import Data.FontGen.Render
 import System.FilePath
-import qualified Ziphil.FontGen.Vekos as Vekos
+import qualified Ziphil.FontGen.Interface as Interface
 
 
 main :: IO ()
-main = do
-  generateAll def Vekos.regularInfo
-  generateAll def Vekos.boldInfo
-  generateAll def Vekos.regularCondensedInfo
-  generateAll def Vekos.boldCondensedInfo
-  writeStrings Vekos.regularInfo
-  writeStrings Vekos.boldInfo
-  writeStrings Vekos.regularCondensedInfo
-  writeStrings Vekos.boldCondensedInfo
-
-writeStrings :: FontInfo -> IO ()
-writeStrings info = renderStrings option info strings
+main = Interface.start generateOption renderOption strings
   where
-    strings = ["gûbumés e tél a kìn,", "pâríqèz a vahax,", "li fay cali jodog."]
-    option = with &~ do
+    strings = ["gûbùmês e tél a kîn!", "pâríqèz a vâháx?", "lì ʻfày cali jôdòg.", "«â, e yer-fitul al'atis.»"]
+    generateOption = def
+    renderOption = with &~ do
       fileName .= "test"
       lineGap .= 200
       scaleRate .= 0.05
---
+++ source/Ziphil/FontGen/Interface.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+
+module Ziphil.FontGen.Interface
+  ( start
+  )
+where
+
+import Data.FontGen hiding (start)
+import Data.FontGen.FontType
+import Data.FontGen.Render
+import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Console.Pretty
+import System.IO
+import Text.Regex.Posix
+import qualified Ziphil.FontGen.Vekos as Vekos
+
+
+start :: GenerateOption -> RenderOption -> [String] -> IO ()
+start generateOption renderOption strings = do
+  flushStr $ colorInput "<?> "
+  line <- getLine
+  flushStrLn ""
+  mapM (generateAll' generateOption) $ parseInfos line
+  mapM (flip (renderStrings' renderOption) strings) $ parseInfos line
+  flushStrLn $ colorMessage "@ Done."
+
+generateAll' :: GenerateOption -> FontInfo -> IO ()
+generateAll' option info = do
+  generateAll option info
+  flushStrLn $ colorMessage $ "@ Generating: " ++ info ^. fullName
+
+renderStrings' :: RenderOption -> FontInfo -> [String] -> IO ()
+renderStrings' option info strings = do
+  renderStrings option info strings 
+  flushStrLn $ colorMessage $ "@ Rendering: " ++ info ^. fullName
+
+flushStr :: String -> IO ()
+flushStr string = putStr string >> hFlush stdout
+
+flushStrLn :: String -> IO ()
+flushStrLn string = putStrLn string >> hFlush stdout
+
+colorMessage :: Pretty a => a -> a
+colorMessage = color Cyan
+
+colorInput :: Pretty a => a -> a
+colorInput = color Yellow
+  
+parseInfos :: String -> [FontInfo]
+parseInfos string = Map.elems $ Map.filterWithKey check wholeInfos
+  where
+    check key info = key =~ ("^" ++ string ++ "$")
+
+wholeInfos :: Map String FontInfo
+wholeInfos = Map.fromList list
+  where
+    list =
+      [ ("Vr", Vekos.regularInfo)
+      , ("Vb", Vekos.boldInfo)
+      , ("Vt", Vekos.thinInfo)
+      , ("Vrc", Vekos.regularCondensedInfo)
+      , ("Vbc", Vekos.boldCondensedInfo)
+      , ("Vtc", Vekos.thinCondensedInfo)
+      , ("Vre", Vekos.regularExtendedInfo)
+      , ("Vbe", Vekos.boldExtendedInfo)
+      , ("Vte", Vekos.thinExtendedInfo)
+      ]
