120d9f6fe4da659a0dfbceba924179d599988e02
Ziphil <yfe23457@nifty.com>
2019-04-21 23:49:39 +0900

    リファクタリング

--- source/Ziphil/FontGen/Vekos.hs
+++ source/Ziphil/FontGen/Vekos.hs
@@ -1,4 +1,5 @@
---
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
 
 
 module Ziphil.FontGen.Vekos
@@ -24,6 +25,11 @@ import Data.Reflection
 import Data.Version
 
 
+config :: Setter FontInfo FontInfo () Config
+config = sets config'
+  where
+    config' func = (metrics .~ give @Config (func ()) Param.metrics) . (glyphs .~ give (func ()) Glyph.glyphs)
+
 commonState :: State FontInfo ()
 commonState = do
   family .= "Vekos"
@@ -34,112 +40,76 @@ regularInfo :: FontInfo
 regularInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Regular
-  metrics .= give regularConfig Param.metrics
-  glyphs .= give regularConfig Glyph.glyphs
-
-regularConfig :: Config
-regularConfig = with &~ do
-  weightConst .= 1
-  stretchConst .= 1
+  config .= with &~ do
+    weightConst .= 1
+    stretchConst .= 1
 
 boldInfo :: FontInfo
 boldInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Bold
-  metrics .= give boldConfig Param.metrics
-  glyphs .= give boldConfig Glyph.glyphs
-
-boldConfig :: Config
-boldConfig = with &~ do
-  weightConst .= 1.5
-  stretchConst .= 1
+  config .= with &~ do
+    weightConst .= 1.5
+    stretchConst .= 1
 
 thinInfo :: FontInfo
 thinInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Thin
-  metrics .= give thinConfig Param.metrics
-  glyphs .= give thinConfig Glyph.glyphs
-
-thinConfig :: Config
-thinConfig = with &~ do
-  weightConst .= 0.5
-  stretchConst .= 1
+  config .= with &~ do
+    weightConst .= 0.5
+    stretchConst .= 1
 
 regularCondensedInfo :: FontInfo
 regularCondensedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Regular
     stretch .= Condensed
-  metrics .= give regularCondensedConfig Param.metrics
-  glyphs .= give regularCondensedConfig Glyph.glyphs
-
-regularCondensedConfig :: Config
-regularCondensedConfig = with &~ do
-  weightConst .= 1
-  stretchConst .= 0.85
+  config .= with &~ do
+    weightConst .= 1
+    stretchConst .= 0.85
 
 boldCondensedInfo :: FontInfo
 boldCondensedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Bold
     stretch .= Condensed
-  metrics .= give boldCondensedConfig Param.metrics
-  glyphs .= give boldCondensedConfig Glyph.glyphs
-
-boldCondensedConfig :: Config
-boldCondensedConfig = with &~ do
-  weightConst .= 1.5
-  stretchConst .= 0.85
+  config .= with &~ do
+    weightConst .= 1.5
+    stretchConst .= 0.85
 
 thinCondensedInfo :: FontInfo
 thinCondensedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Thin
     stretch .= Condensed
-  metrics .= give thinCondensedConfig Param.metrics
-  glyphs .= give thinCondensedConfig Glyph.glyphs
-
-thinCondensedConfig :: Config
-thinCondensedConfig = with &~ do
-  weightConst .= 0.5
-  stretchConst .= 0.85
+  config .= with &~ do
+    weightConst .= 0.5
+    stretchConst .= 0.85
 
 regularExtendedInfo :: FontInfo
 regularExtendedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Regular
     stretch .= Extended
-  metrics .= give regularExtendedConfig Param.metrics
-  glyphs .= give regularExtendedConfig Glyph.glyphs
-
-regularExtendedConfig :: Config
-regularExtendedConfig = with &~ do
-  weightConst .= 1
-  stretchConst .= 1.25
+  config .= with &~ do
+    weightConst .= 1
+    stretchConst .= 1.25
 
 boldExtendedInfo :: FontInfo
 boldExtendedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Bold
     stretch .= Extended
-  metrics .= give boldExtendedConfig Param.metrics
-  glyphs .= give boldExtendedConfig Glyph.glyphs
-
-boldExtendedConfig :: Config
-boldExtendedConfig = with &~ do
-  weightConst .= 1.5
-  stretchConst .= 1.25
+  config .= with &~ do
+    weightConst .= 1.5
+    stretchConst .= 1.25
 
 thinExtendedInfo :: FontInfo
 thinExtendedInfo = with &~ commonState &~ do
   style .= with &~ do
     weight .= Thin
     stretch .= Extended
-  metrics .= give thinExtendedConfig Param.metrics
-  glyphs .= give thinExtendedConfig Glyph.glyphs
-
-thinExtendedConfig :: Config
-thinExtendedConfig = with &~ do
-  weightConst .= 0.5
-  stretchConst .= 1.25
+  config .= with &~ do
+    weightConst .= 0.5
+    stretchConst .= 1.25
