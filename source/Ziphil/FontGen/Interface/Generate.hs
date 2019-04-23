965c010d32375fb9641eccd56bf9f6964df7be63
Ziphil <yfe23457@nifty.com>
2019-04-23 09:35:33 +0900

    インターフェースの表示を改善

--- source/Ziphil/FontGen/Interface/Generate.hs
+++ source/Ziphil/FontGen/Interface/Generate.hs
@@ -11,6 +11,7 @@ import Data.FontGen.FontType
 import Data.FontGen.Render
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Text.Printf
 import Text.Regex.Posix
 import Ziphil.FontGen.Interface.Util
 import qualified Ziphil.FontGen.Vekos as Vekos
@@ -20,20 +21,35 @@ start :: GenerateOption -> RenderOption -> IO ()
 start generateOption renderOption = do
   flushStr $ colorInput "<?> "
   line <- getLine
+  infos <- return $ parseInfos line
+  infosWithIndex <- return $ zipWith (\info i -> (info, (i, length infos))) infos [1 ..]
   flushStrLn ""
-  mapM (generateAll' generateOption) $ parseInfos line
-  mapM (renderStrings' renderOption) $ parseInfos line
+  prepareProgress "Generating"
+  mapM (generateAll' generateOption) infosWithIndex
+  prepareProgress "Rendering"
+  mapM (renderStrings' renderOption) infosWithIndex
   flushStrLn $ colorMessage "@ Done."
 
-generateAll' :: GenerateOption -> FontInfo -> IO ()
-generateAll' option info = do
-  generateAll option info
-  flushStrLn $ colorMessage $ "@ Generating: " <> info ^. fullName
+prepareProgress :: String -> IO ()
+prepareProgress message = do
+  flushStrLn ""
+  updateProgress message "" (0, 0)
 
-renderStrings' :: RenderOption -> FontInfo -> IO ()
-renderStrings' option info = do
+updateProgress :: String -> String -> (Int, Int) -> IO ()
+updateProgress message item (i, size) = do
+  cursorUpLine 1
+  clearLine
+  flushStrLn $ colorMessage $ "@ " <> message <> ": " <> printf "[%2d/%2d]" i size <> " " <> item
+
+generateAll' :: GenerateOption -> (FontInfo, (Int, Int)) -> IO ()
+generateAll' option (info, progress) = do
+  updateProgress "Generating" (info ^. fullName) progress
+  generateAll option info
+
+renderStrings' :: RenderOption -> (FontInfo, (Int, Int)) -> IO ()
+renderStrings' option (info, progress) = do
+  updateProgress "Rendering" (info ^. fullName) progress
   renderStrings option info
-  flushStrLn $ colorMessage $ "@ Rendering: " <> info ^. fullName
   
 parseInfos :: String -> [FontInfo]
 parseInfos string = Map.elems $ Map.filterWithKey check wholeInfos
