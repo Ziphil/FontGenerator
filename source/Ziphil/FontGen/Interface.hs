a8f397a28eef034c5c21c2124aa1da1aa8f86a8e
Ziphil <yfe23457@nifty.com>
2019-07-15 03:42:31 +0900

    実行時にフォント生成を行うか選べるよう変更

--- source/Ziphil/FontGen/Interface.hs
+++ source/Ziphil/FontGen/Interface.hs
@@ -9,9 +9,26 @@ where
 import Data.FontGen
 import Data.FontGen.FontType
 import Data.FontGen.Render
+import Data.Map (Map)
+import qualified Data.Map as Map
 import qualified Ziphil.FontGen.Interface.Generate as Generate
+import qualified Ziphil.FontGen.Vekos as Vekos
 
 
 main :: GenerateOption -> RenderOption -> IO ()
-main generateOption renderOption = do
-  Generate.main generateOption renderOption
+main generateOption renderOption = Generate.main generateOption renderOption fonts
+
+fonts :: Map String Font
+fonts = Map.fromList list
+  where
+    list =
+      [ ("Vr", Vekos.regularFont)
+      , ("Vb", Vekos.boldFont)
+      , ("Vt", Vekos.thinFont)
+      , ("Vrc", Vekos.regularCondensedFont)
+      , ("Vbc", Vekos.boldCondensedFont)
+      , ("Vtc", Vekos.thinCondensedFont)
+      , ("Vre", Vekos.regularExtendedFont)
+      , ("Vbe", Vekos.boldExtendedFont)
+      , ("Vte", Vekos.thinExtendedFont)
+      ]
--- source/Ziphil/FontGen/Interface/Generate.hs
+++ source/Ziphil/FontGen/Interface/Generate.hs
@@ -6,6 +6,7 @@ module Ziphil.FontGen.Interface.Generate
   )
 where
 
+import Control.Monad
 import Data.FontGen
 import Data.FontGen.FontType
 import Data.FontGen.Render
@@ -14,21 +15,61 @@ import qualified Data.Map as Map
 import Text.Printf
 import Text.Regex.Posix
 import Ziphil.FontGen.Interface.Util
-import qualified Ziphil.FontGen.Vekos as Vekos
 
 
-main :: GenerateOption -> RenderOption -> IO ()
-main generateOption renderOption = do
-  flushStr $ colorInput "<?> "
-  line <- getLine
-  fonts <- return $ parseFonts line
-  fontsWithIndex <- return $ zipWith (\font i -> (font, (i, length fonts))) fonts [1 ..]
+main :: GenerateOption -> RenderOption -> Map String Font -> IO ()
+main generateOption renderOption fonts = do
+  simple <- inputSimple
+  fonts <- inputFonts fonts
   flushStrLn ""
-  prepareProgress "Generating"
-  mapM (generateAll' generateOption) fontsWithIndex
+  mainLoop generateOption renderOption simple $ makeIndexedFonts fonts
+
+inputSimple :: IO Bool
+inputSimple = do
+  flushStr $ colorInput "<?> Simple -> "
+  input <- getLine
+  case input of
+    "" -> return True
+    'Y' : _ -> return True
+    'N' : _ -> return False
+    _ -> return True
+
+inputFonts :: Map String Font -> IO [Font]
+inputFonts fonts = do
+  flushStr $ colorInput "<?> Name -> "
+  input <- getLine
+  parseFonts fonts input
+
+parseFonts :: Map String Font -> String -> IO [Font]
+parseFonts fonts string = return . Map.elems $ Map.filterWithKey check fonts
+  where
+    check key font = key =~ ("^" <> string <> "$")
+
+type IndexedFont = (Font, (Int, Int))
+
+makeIndexedFonts :: [Font] -> [IndexedFont]
+makeIndexedFonts fonts = zipWith make fonts [1 ..]
+  where
+    make font index = (font, (index, length fonts))
+
+mainLoop :: GenerateOption -> RenderOption -> Bool -> [IndexedFont] -> IO ()
+mainLoop generateOption renderOption simple indexedFonts = do
+  unless simple . void $ do
+    prepareProgress "Generating"
+    mapM (generateAll' generateOption) indexedFonts
   prepareProgress "Rendering"
-  mapM (renderStrings' renderOption) fontsWithIndex
+  mapM (renderStrings' renderOption) indexedFonts
   flushStrLn $ colorMessage "@ Done."
+
+generateAll' :: GenerateOption -> IndexedFont -> IO ()
+generateAll' option (font, progress) = do
+  updateProgress "Generating" (font ^. fullName) progress
+  generateAll option font
+
+renderStrings' :: RenderOption -> IndexedFont -> IO ()
+renderStrings' option (font, progress) = do
+  updateProgress "Rendering" (font ^. fullName) progress
+  renderStrings option font
 
 prepareProgress :: String -> IO ()
 prepareProgress message = do
@@ -36,37 +77,7 @@ prepareProgress message = do
   updateProgress message "" (0, 0)
 
 updateProgress :: String -> String -> (Int, Int) -> IO ()
-updateProgress message item (i, size) = do
+updateProgress message item (index, size) = do
   cursorUpLine 1
   clearLine
-  flushStrLn $ colorMessage $ "@ " <> message <> ": " <> printf "[%2d/%2d]" i size <> " " <> item
-
-generateAll' :: GenerateOption -> (Font, (Int, Int)) -> IO ()
-generateAll' option (font, progress) = do
-  updateProgress "Generating" (font ^. fullName) progress
-  generateAll option font
-
-renderStrings' :: RenderOption -> (Font, (Int, Int)) -> IO ()
-renderStrings' option (font, progress) = do
-  updateProgress "Rendering" (font ^. fullName) progress
-  renderStrings option font
-  
-parseFonts :: String -> [Font]
-parseFonts string = Map.elems $ Map.filterWithKey check wholeFonts
-  where
-    check key font = key =~ ("^" <> string <> "$")
-
-wholeFonts :: Map String Font
-wholeFonts = Map.fromList list
-  where
-    list =
-      [ ("Vr", Vekos.regularFont)
-      , ("Vb", Vekos.boldFont)
-      , ("Vt", Vekos.thinFont)
-      , ("Vrc", Vekos.regularCondensedFont)
-      , ("Vbc", Vekos.boldCondensedFont)
-      , ("Vtc", Vekos.thinCondensedFont)
-      , ("Vre", Vekos.regularExtendedFont)
-      , ("Vbe", Vekos.boldExtendedFont)
-      , ("Vte", Vekos.thinExtendedFont)
-      ]
+  flushStrLn $ colorMessage $ "@ " <> message <> ": " <> printf "[%2d/%2d]" index size <> " " <> item
