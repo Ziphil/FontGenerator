7916ffa2eb3fe59413e6dc17160899f4c79f470d
Ziphil <yfe23457@nifty.com>
2019-07-24 21:28:36 +0900

    リファクタリング

--- application/Main.hs
+++ application/Main.hs
@@ -14,15 +14,14 @@ main = Interface.main generateOption renderOption fonts
   where
     generateOption = def
     renderOption = with &~ do
-      strings .= 
-        [ " gÛbÙmêS e TéL a KîN! "
-        , " PâRíQèZ a VâHáX? "
-        , " lÌ ʻfÀy CaLi JÔdÒg. "
-        , " «â, S'e YeR-fItUl….» "
-        , " A 2·4 tE 31:05:2019. "
-        , " cAtEs — OmêL — a'L. "
-        , " zA sWA Re Lwe "
-        ]
-      fileName .= "test"
+      strings <>= ["gÛbÙmêS e TéL a KîN!" , "PâRíQèZ a VâHáX?", "lÌ ʻfÀy CaLi JÔdÒg."]
+      strings <>= ["«â, S'e YeR-fItUl….»"]
+      strings <>= ["A 2·4 tE 31:05:2019."]
+      strings <>= ["cAtEs — OmêL — a'L."]
+      strings <>= ["zA sWA Re Lwe"]
       lineGap .= 200
       scaleRate .= 0.05
+      modifyStrings
+
+modifyStrings :: State RenderOption ()
+modifyStrings = strings %= map ((<> " ") . (" " <>))
